810a422b3e65d9e71816cf52457b3521ca03ce08
rezonmain <adiazgomez101@gmail.com>
2022-06-06 22:02:00 -0700

    added cool grid pattern

--- src/Snake/lib/utils.js
+++ src/Snake/lib/utils.js
@@ -1,8 +1,13 @@
+import { createPortal } from 'react-dom';
+
 const BLOCK = 35;
+const GREEN = 'green';
+const GREEN_ACCENT = '#24BF4B';
 
 export function setUpCanvas() {
 	const canvas = document.getElementById('snake-board');
 	const grid = calculateGrid(canvas.width, canvas.height);
+	drawBorders(canvas, grid);
 	drawGrid(canvas, grid);
 }
 
@@ -19,14 +24,29 @@ function calculateGrid(width, height) {
 	};
 }
 
+function drawBorders(canvas, grid) {
+	const ctx = canvas.getContext('2d');
+	ctx.fillStyle = GREEN;
+
+	// Left and right borders:
+	ctx.fillRect(0, 0, grid.xOffset, canvas.height);
+	ctx.fillRect(canvas.width - grid.xOffset, 0, grid.xOffset, canvas.height);
+
+	// Top and bottom borders:
+	ctx.fillRect(0, 0, canvas.width, grid.yOffset);
+	ctx.fillRect(0, canvas.height - grid.yOffset, canvas.width, grid.yOffset);
+}
+
 function drawGrid(canvas, grid) {
 	const ctx = canvas.getContext('2d');
+	let alt = true;
+	ctx.lineWidth = '1px';
 	for (let i = grid.xOffset; i < canvas.width - grid.xOffset; i += 35) {
 		for (let j = grid.yOffset; j < canvas.height - grid.yOffset; j += 35) {
-			ctx.rect(i, j, 35, 35);
-			ctx.strokeStyle = 'black';
-			ctx.lineWidth = '1px';
-			ctx.stroke();
+			ctx.fillStyle = alt ? GREEN_ACCENT : 'lightgreen';
+			ctx.fillRect(i, j, 35, 35);
+			alt = !alt;
 		}
+		alt = !alt;
 	}
 }
