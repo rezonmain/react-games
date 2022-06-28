7c129e9cf9a058d9a89eaa5482107298b9ea3fde
rezonmain <adiazgomez101@gmail.com>
2022-06-27 20:28:07 -0700

    added tenzies page

--- pages/games/tenzies.js
+++ pages/games/tenzies.js
@@ -1,6 +1,13 @@
-import Tenzies from '../../components/Tenzies/Tenzies';
+import dynamic from 'next/dynamic';
 import HowTo from '../../components/shared/HowTo';
 export default function TenziesPage() {
+	/* Due to generating random numbers, component text change on refresh
+  so if we ssr this component it throws a hydration error, so disable ssr for
+  the tenzies component
+   */
+	const Tenzies = dynamic(() => import('../../components/Tenzies/Tenzies'), {
+		ssr: false,
+	});
 	return (
 		<>
 			<Tenzies />
